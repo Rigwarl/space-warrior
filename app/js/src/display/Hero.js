da44bf4ae8c742e73ea5729973a06b75db0df5e0
Rigwarl <Avips@mail.ru>
2016-10-11 17:56:05 +0300

    add fire method to hero

--- app/js/src/display/Hero.js
+++ app/js/src/display/Hero.js
@@ -1,5 +1,6 @@
-import assetsManager from '../managers/assetsManager';
 import { TILE, HERO_PROPS } from '../config';
+import assetsManager from '../managers/assetsManager';
+import Bullet from './Bullet';
 
 export default class Hero extends createjs.Container {
   constructor() {
@@ -8,7 +9,6 @@ export default class Hero extends createjs.Container {
     this.createPlatform();
     this.createTower();
     this.setProps();
-    this.rotation = -90;
   }
   createTower() {
     this.tower = new createjs.Bitmap(assetsManager.getResult('tank-tower-gray'));
@@ -24,9 +24,22 @@ export default class Hero extends createjs.Container {
   setProps() {
     this.v = 0;
     this.vRot = 0;
+    this.firing = false;
+    this.weaponCd = 0;
+
+    this.rotation = -90;
   }
   rotateTower(rot) {
     this.tower.rotation = 90 + rot;
+  }
+  fire() {
+    this.weaponCd = HERO_PROPS.WEAPON_CD;
+
+    const bullet = new Bullet(this.tower.rotation);
+    bullet.x = this.x + TILE.WIDTH * 0.7 * Math.sin(this.tower.rotation * Math.PI / 180);
+    bullet.y = this.y - TILE.HEIGHT * 0.7 * Math.cos(-this.tower.rotation * Math.PI / 180);
+
+    this.parent.addChild(bullet);
   }
   setActions(actions) {
     if (actions.left) {
@@ -42,10 +55,18 @@ export default class Hero extends createjs.Container {
       this.v += HERO_PROPS.A;
     }
     this.v *= (1 - HERO_PROPS.T);
+
+    this.firing = actions.fire;
   }
   move() {
     this.y += this.v * Math.cos(this.platform.rotation * Math.PI / 180);
     this.x += this.v * Math.sin(-this.platform.rotation * Math.PI / 180);
     this.platform.rotation += this.vRot;
+
+    if (this.weaponCd) {
+      this.weaponCd -= 1;
+    } else if (this.firing) {
+      this.fire();
+    }
   }
 }
